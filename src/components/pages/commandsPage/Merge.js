ac0b9e226162f9eadba1a16ecfbf33cd7b47da45
Radist-electric <radist-electric@yandex.ru>
2020-04-27 22:37:54 +0500

    I added commands into tab Merge

--- src/components/pages/commandsPage/Merge.js
+++ src/components/pages/commandsPage/Merge.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
-import { Row } from 'reactstrap';
+import Multiselect from '../../select/Multiselect';
+
+let gitMerge = [
+  {
+    command: 'git merge [branch]',
+    input: [
+      'Имя ветки, c которой происходит слияние'
+    ],
+    description: 'Влить в активную ветку (та, в которой находимся) изменения из ветки [input]'
+  },
+  {
+    command: 'git mergetool',
+    input: [],
+    description: 'Запустить утилиту для слияния, которая прописана в конфигурации.'
+  },
+  {
+    command: 'git mergetool --tool-help',
+    input: [],
+    description: 'Покажет инструменты, которые могут быть выбраны в качестве инструмента для слияния. Часть инструментов может быть уже доступна для выбора. Другая часть инструментов в данный момент недоступна, но может быть дополнительно установлена.'
+  }
+];
+
 
 export default class Merge extends Component {
   // constructor (props) {
@@ -7,14 +28,10 @@ export default class Merge extends Component {
   // }
   render() {
     return (
-      <Row>
-        <div className="col-12">
-          <h2>Устранение конфликтов</h2>
-        </div>
-        <div className="col-12">
-          <p>Cлияние веток и откат к предыдущему состоянию. Устранение конфликтов слияния.</p>
-        </div>
-      </Row>
+      <>
+        <h2>Устранение конфликтов</h2>
+        <Multiselect data={gitMerge} />
+      </>
     );
 
   }

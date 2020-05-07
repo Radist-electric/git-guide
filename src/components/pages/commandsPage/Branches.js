c4cbffb0e9428afd683da3ec6c20eead79e86a8c
Radist-electric <radist-electric@yandex.ru>
2020-05-07 22:58:21 +0500

    I added a few new commands

--- src/components/pages/commandsPage/Branches.js
+++ src/components/pages/commandsPage/Branches.js
@@ -18,11 +18,30 @@ let gitBranch = [
     description: 'Показать список веток в локальном и удалённом репозиториях. Текущая ветка в локальном репозитории будет выделена звёздочкой и цветом.'
   },
   {
+    command: 'git branch -r',
+    input: [],
+    description: 'Показать список веток в удалённом репозитории.'
+  },
+  {
+    command: 'git branch -d [old_branch]',
+    input: [
+      'Имя удаляемой ветки, например, old_branch'
+    ],
+    description: 'Удалить ветку [input]. Если эта ветка ещё не была слита с текущей веткой, то git предупредит об этом. Необходимо выполнить слияние с помощью git merge или выполнить с помощью команды git branch -D принудительное удаление без слияния с текущей веткой.'
+  },
+  {
+    command: 'git branch -D [old_branch]',
+    input: [
+      'Имя удаляемой ветки, например, old_branch'
+    ],
+    description: 'Принудительно удалить ветку [input], даже если эта ветка ещё не была слита с текущей веткой.'
+  },
+  {
     command: 'git branch [new_branch]',
     input: [
       'Имя новой ветки, например, new_branch'
     ],
-    description: 'Создать новую ветку с указанным именем [input] на текущем коммите.'
+    description: 'Создать на текущем коммите новую ветку с указанным именем [input].'
   },
   {
     command: 'git branch [new_branch] [16fce82]',
@@ -54,10 +73,10 @@ let gitCheckout = [
 const Branches = () => {
   return (
     <>
-    <h2>Работа с ветками</h2>
-    <Multiselect data={gitBranch} />
-    <Multiselect data={gitCheckout} />
-  </>
+      <h2>Работа с ветками</h2>
+      <Multiselect data={gitBranch} />
+      <Multiselect data={gitCheckout} />
+    </>
   )
 }
 
